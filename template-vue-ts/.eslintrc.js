9850edf6855a2c7d06a187c8a20ed426e88bc2cd
tiantingrui <976912095@qq.com>
2022-04-25 16:12:15 +0800

    feat: 完善 vue-ts-template

---
+++ template-vue-ts/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    'vue/setup-compiler-macros': true,
+  },
+  parser: 'vue-eslint-parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/no-multiple-template-root': 'off',
+    // 'no-undef': 'off',
+  },
+};
